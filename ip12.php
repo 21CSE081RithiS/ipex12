fbb4ebecb368a461d69819d964c733bf55355f17
21CSE081RithiS <111380374+21CSE081RithiS@users.noreply.github.com>
2023-04-23 14:40:49 +0530

    Add files via upload

--- ip12.php
+++ ip12.php
@@ -1,65 +1,51 @@
 <!DOCTYPE html>
-  
-<head>
-    <script>
-        function loadXMLDoc() {
-            var xmlhttp = new XMLHttpRequest();
-            xmlhttp.onreadystatechange = function () {
-  
-                // Request finished and response 
-                // is ready and Status is "OK"
-                if (this.readyState == 4 && this.status == 200) {
-                    Details(this);
-                }
-            };
-  
-            // employee.xml is the external xml file
-            xmlhttp.open("GET", "product.xml", true);
-            xmlhttp.send();
-        }
-  
-        function Details(xml) {
-            var i;
-            var xmlDoc = xml.responseXML;
-            var table =
-                `<tr><th>p_name</th>
-                    <th>p_rate</th>
-                    <th>p_warranty</th></tr>`;
-            var x = xmlDoc.getElementsByTagName("product");
-  
-            // Start to fetch the data by using TagName 
-            for (i = 0; i < x.length; i++) {
-                table += "<tr><td>" +
-                    x[i].getElementsByTagName("p_name")[0]
-                    .childNodes[0].nodeValue + "</td><td>" +
-                    x[i].getElementsByTagName("p_rate")[0]
-                    .childNodes[0].nodeValue + "</td><td>" +
-                    x[i].getElementsByTagName("p_warranty")[0]
-                    .childNodes[0].nodeValue + "</td><td>" ;
-            }
-  
-            // Print the xml data in table form
-            document.getElementById("id").innerHTML = table;
-        }
-    </script>
-</head>
-  
+<html>
+<style>
+table,th,td {
+  border : 1px solid black;
+  border-collapse: collapse;
+  class="centre";
+}
+th,td {
+  padding: 5px;
+}
+</style>
 <body>
-<label for="product">Choose a product:</label>
 
-<select id="products" name="products">
-    <option value="Computer">Computer</option>
-  <option value="Speaker<">Speaker<</option>
-  <option value="Headset">Headset</option>
-  <option value="Mobile">Mobile</option>
-</select>
-        <button style="darkgoldenrod " type="button" class="button" onclick="loadXMLDoc()">
-            Get details
-        </button>
-    
-      
-    <br><br>
-    <table id="id"></table>
+<h5 style=text-allign:"centre;">click here to get product details</h5>
+<centre>
+<button type="button" onclick="loadDoc()">Get product details</button>
+<br><br>
+</centre>
+<table id="demo"></table>
+
+<script>
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      myFunction(this);
+    }
+  };
+  xhttp.open("GET", "product.xml", true);
+  xhttp.send();
+}
+function myFunction(xml) {
+  var i;
+  var xmlDoc = xml.responseXML;
+  var table="<tr><th>product id</th><th>product name</th><th>product rate</th><th>warranty</th></tr>";
+  var x = xmlDoc.getElementsByTagName("p");
+  for (i = 0; i <x.length; i++) { 
+    table += "<tr><td>" +
+    x[i].getElementsByTagName("pid")[0].childNodes[0].nodeValue +
+    "</td><td>" + x[i].getElementsByTagName("p_name")[0].childNodes[0].nodeValue +
+    "</td><td>" + x[i].getElementsByTagName("p_rate")[0].childNodes[0].nodeValue +
+    "</td><td>" + x[i].getElementsByTagName("p_warranty")[0].childNodes[0].nodeValue +
+    "</td></tr>";
+  }
+  document.getElementById("demo").innerHTML = table;
+}
+</script>
+
 </body>
-  
 </html>
